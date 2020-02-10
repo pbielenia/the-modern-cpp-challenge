833e801b08f401e899b4f175f7c005e501456ab5
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-02-10 17:55:18 +0100

    1-3: works with std::accumulate

--- 1-3-najmniejsza_wspolna_wielokrotnosc/main.cpp
+++ 1-3-najmniejsza_wspolna_wielokrotnosc/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <numeric>
 #include <cstdlib>
+#include <vector>
 #include <utility>
 
-int lcm_iterative(int const a, int const b)
+int lcm_iterative(int a, int b)
 {
     auto _c = a * b;
     auto _a = a;
@@ -18,7 +19,7 @@ int lcm_iterative(int const a, int const b)
     return _c / _a;
 }
 
-int lcm_iterative(const std::pair<int const, int const>& numbers)
+int lcm_iterative(const std::pair<int, int>& numbers)
 {
     return lcm_iterative(numbers.first, numbers.second);
 }
@@ -35,20 +36,33 @@ int lcm_recursive(const std::pair<int const, int const>& numbers)
     return lcm_recursive(numbers.first, numbers.second);
 }
 
-int lcd_using_gcd(std::pair<int const, int const>& numbers)
+int lcd_using_gcd(int a, int b)
 {
-    return std::abs(numbers.first * numbers.second)
-        / std::gcd(numbers.first, numbers.second);
+    return std::abs(a * b)
+        / std::gcd(a, b);
 }
 
+int lcd_using_gcd(std::pair<int const, int const>& numbers)
+{
+    return lcd_using_gcd(numbers.first, numbers.second);
+}
 
+template<class InputIter>
+int lcmr(InputIter first, InputIter last)
+{
+    return std::accumulate(first, last, 1, static_cast<int(*)(int, int)>(&lcm_iterative));
+}
 
 int main()
 {
     std::pair<int const, int const> numbers(-48, 14);
+    std::vector<int> vector_of_numbers;
+    vector_of_numbers.push_back(48);
+    vector_of_numbers.push_back(14);
 
     std::cout << "STL : " << std::lcm(numbers.first, numbers.second) << std::endl;
     std::cout << "Euclides iterative : " << lcm_iterative(numbers) << std::endl;
     std::cout << "Using gcd : " << lcd_using_gcd(numbers) << std::endl;
     // std::cout << "Euclides recursive : " << lcm_recursive(numbers) << std::endl;
+    std::cout << "Multiple inputs : " << lcmr(vector_of_numbers.begin(), vector_of_numbers.end()) << std::endl;
 }
