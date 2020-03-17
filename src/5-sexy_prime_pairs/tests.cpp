f3bd2fc8efd4730873c622b0374ad6bb4a09deb4
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-17 19:43:37 +0100

    Add tests to excercise 5

--- src/5-sexy_prime_pairs/tests.cpp
+++ src/5-sexy_prime_pairs/tests.cpp
@@ -10,6 +10,13 @@ int main(int argc, char** argv)
 
 using namespace testing;
 
-TEST(LcmUsingGcd, ReturnsCorrectFrom5And7) {
-    ASSERT_THAT(0, 0);
+TEST(GetSexyPairs, ReturnsCorrectFrom15) {
+    ASSERT_THAT(get_sexy_pairs(15), ElementsAre(Pair(5, 11), Pair(7, 13)));
+}
+
+TEST(GetSexyPairs, ReturnsCorrectFrom100) {
+    ASSERT_THAT(get_sexy_pairs(100), ElementsAre(Pair(5, 11), Pair(7, 13),
+        Pair(11,17), Pair(13,19), Pair(17,23), Pair(23,29), Pair(31,37),
+        Pair(37,43), Pair(41,47), Pair(47,53), Pair(53,59), Pair(61,67),
+        Pair(67,73), Pair(73,79), Pair(83,89)));
 }
