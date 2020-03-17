c886525bd736c37af811bba474e4e501f69af4fb
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-17 19:28:25 +0100

    Add tests to excercise 4

--- src/4-largest_prime_smaller_than_given_number/tests.cpp
+++ src/4-largest_prime_smaller_than_given_number/tests.cpp
@@ -10,6 +10,26 @@ int main(int argc, char** argv)
 
 using namespace testing;
 
-TEST(LcmUsingGcd, ReturnsCorrectFrom5And7) {
-    ASSERT_THAT(0, 0);
+TEST(FindClosestPrime, ReturnsCorrectFrom10) {
+    ASSERT_THAT(find_closest_prime(10), 7);
+}
+
+TEST(FindClosestPrime, ReturnsCorrectFrom100) {
+    ASSERT_THAT(find_closest_prime(100), 97);
+}
+
+TEST(FindClosestPrime, ReturnsCorrectFrom600) {
+    ASSERT_THAT(find_closest_prime(600), 599);
+}
+
+TEST(SieveOfEratosthenes, ReturnsCorrectFrom10) {
+    ASSERT_THAT(sieve_of_eratosthenes(10).back(), 7);
+}
+
+TEST(SieveOfEratosthenes, ReturnsCorrectFrom100) {
+    ASSERT_THAT(sieve_of_eratosthenes(100).back(), 97);
+}
+
+TEST(SieveOfEratosthenes, ReturnsCorrectFrom600) {
+    ASSERT_THAT(sieve_of_eratosthenes(600).back(), 599);
 }
