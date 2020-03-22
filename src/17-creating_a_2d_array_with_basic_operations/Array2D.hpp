988fe97f69c7076ee07a7a57fdbb90e7c71bfcfc
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-22 17:18:36 +0100

    17 - refactor with adding custom exceptions

--- src/17-creating_a_2d_array_with_basic_operations/Array2D.hpp
+++ src/17-creating_a_2d_array_with_basic_operations/Array2D.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include "exceptions.hpp"
 
 #include <iostream>
 
@@ -11,9 +12,25 @@ public:
         : elements(number_of_elements) { }
 
     Array2D(std::initializer_list<T> elements)
-        : elements(elements) {  }
+        : elements(elements)
+    {
+        check_length_after_init(); 
+        
+    }
 
 private:
+    void check_length_after_init()
+    {
+        if (number_of_elements < elements.size()) {
+            throw PassedElementsExceedArraySizeException(
+                elements.size(), number_of_elements);
+            
+        } else if (number_of_elements > elements.size()) {
+            throw PassedElementsDoNotReachArraySizeException(
+                elements.size(), number_of_elements);
+        }
+    }
+
     unsigned number_of_elements{R * C};
     std::vector<T> elements;
 
--- src/17-creating_a_2d_array_with_basic_operations/tests.cpp
+++ src/17-creating_a_2d_array_with_basic_operations/tests.cpp
@@ -10,3 +10,13 @@ TEST(Array2D, ConstructsFromTemplateWithTypeRowsAndColumnsNumber) {
 TEST(Array2D, ConstructsWithGivenElements) {
     Array2D<char, 2, 3>{'a', 'b', 'c', 'd', 'e', 'f'};
 }
+
+TEST(Array2D, ThrowsWhileConstructingWithTooMuchElements) {
+    ASSERT_THROW((Array2D<char, 2, 3>{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
+                 PassedElementsExceedArraySizeException);
+}
+
+TEST(Array2D, ThrowsWhileConstructingWithTooLittleElements) {
+    ASSERT_THROW((Array2D<char, 2, 3>{'a', 'b', 'c', 'd', 'e'}),
+                 PassedElementsDoNotReachArraySizeException);
+}
