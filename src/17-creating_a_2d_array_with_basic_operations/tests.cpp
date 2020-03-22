0e7ef6addc03f39b68d98212fad3298692f53feb
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-22 15:29:38 +0100

    17 - remove main() in tests, link to gtest_main instead1

--- src/17-creating_a_2d_array_with_basic_operations/tests.cpp
+++ src/17-creating_a_2d_array_with_basic_operations/tests.cpp
@@ -1,10 +1,5 @@
 #include <gmock/gmock.h>
 #include "Array2D.hpp"
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
 
 using namespace testing;
 
