ff19b8f89482765f3136b5a256a62c62ac7c5446
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-22 15:17:33 +0100

    17 - create dir and set up CMake

---
+++ src/17-creating_a_2d_array_with_basic_operations/tests.cpp
@@ -0,0 +1,13 @@
+#include <gmock/gmock.h>
+#include "Array2D.hpp"
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+using namespace testing;
+
+TEST(Array2D, Exists) {
+
+}
