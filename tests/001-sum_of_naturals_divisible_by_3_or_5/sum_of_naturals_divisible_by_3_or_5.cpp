df4e01afe3b4209396def125b95067215595b431
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-06-28 15:56:18 +0200

    Rename first two problems tests

--- tests/001-sum_of_naturals_divisible_by_3_or_5/sum_of_naturals_divisible_by_3_or_5.cpp
+++ tests/001-sum_of_naturals_divisible_by_3_or_5/sum_of_naturals_divisible_by_3_or_5.cpp
@@ -3,22 +3,22 @@
 
 using namespace testing;
 
-TEST(Problem1, Returns0When0Passed) {
+TEST(SumOfNaturalsDivisibleBy3Or5, Returns0When0Passed) {
   ASSERT_THAT(sum_of_naturals_divisible_by_3_or_5(0), Eq(0));
 }
 
-TEST(Problem1, Returns3When3Passed) {
+TEST(SumOfNaturalsDivisibleBy3Or5, Returns3When3Passed) {
   ASSERT_THAT(sum_of_naturals_divisible_by_3_or_5(3), Eq(3));
 }
 
-TEST(Problem1, Returns8When5Passed) {
+TEST(SumOfNaturalsDivisibleBy3Or5, Returns8When5Passed) {
   ASSERT_THAT(sum_of_naturals_divisible_by_3_or_5(5), Eq(8));
 }
 
-TEST(Problem1, Returns33When10Passed) {
+TEST(SumOfNaturalsDivisibleBy3Or5, Returns33When10Passed) {
   ASSERT_THAT(sum_of_naturals_divisible_by_3_or_5(10), Eq(33));
 }
 
-TEST(Problem1, Returns98When20Passed) {
+TEST(SumOfNaturalsDivisibleBy3Or5, Returns98When20Passed) {
   ASSERT_THAT(sum_of_naturals_divisible_by_3_or_5(20), Eq(98));
 }
--- tests/002-greatest_common_divisor/greatest_common_divisor.cpp
+++ tests/002-greatest_common_divisor/greatest_common_divisor.cpp
@@ -4,26 +4,26 @@
 
 using namespace testing;
 
-TEST(Problem1, RecursiveReturnsCorrectFrom5And7) {
+TEST(GreatestCommonDivisor, RecursiveReturnsCorrectFrom5And7) {
   ASSERT_THAT(gcd_recursive(5, 7), std::gcd(5, 7));
 }
 
-TEST(Problem1, RecursiveReturnsCorrectFrom10And20) {
+TEST(GreatestCommonDivisor, RecursiveReturnsCorrectFrom10And20) {
   ASSERT_THAT(gcd_recursive(10, 20), std::gcd(10, 20));
 }
 
-TEST(Problem1, RecursiveReturnsCorrectFrom78And12) {
+TEST(GreatestCommonDivisor, RecursiveReturnsCorrectFrom78And12) {
   ASSERT_THAT(gcd_recursive(78, 12), std::gcd(78, 12));
 }
 
-TEST(Problem1, IterativeReturnsCorrectFrom5And7) {
+TEST(GreatestCommonDivisor, IterativeReturnsCorrectFrom5And7) {
   ASSERT_THAT(gcd_iterative(5, 7), std::gcd(5, 7));
 }
 
-TEST(Problem1, IterativeReturnsCorrectFrom10And20) {
+TEST(GreatestCommonDivisor, IterativeReturnsCorrectFrom10And20) {
   ASSERT_THAT(gcd_iterative(10, 20), std::gcd(10, 20));
 }
 
-TEST(Problem1, IterativeReturnsCorrectFrom78And12) {
+TEST(GreatestCommonDivisor, IterativeReturnsCorrectFrom78And12) {
   ASSERT_THAT(gcd_iterative(78, 12), std::gcd(78, 12));
 }
