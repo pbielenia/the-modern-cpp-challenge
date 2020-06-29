61847c4bdb47304c3e9b56a8acb11b291b69d8da
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-06-29 18:37:34 +0200

    023 - init

---
+++ tests/023-binary_to_string_conversion/binary_to_string_conversion.cpp
@@ -0,0 +1,10 @@
+#include "binary_to_string_conversion/BinToStrConverter.hpp"
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+
+TEST(BinToStrConverter, ConstructsItself)
+{
+    BinToStrConverter converter();
+}
