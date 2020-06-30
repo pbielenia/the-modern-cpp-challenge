24cab2c1b4685cde213af8f11808ba39d7b1671c
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-06-30 22:09:41 +0200

    Create script file and to do list

---
+++ scripts/create_project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+print('Hello World!')
+
+# get user input as arguments
+# create:
+#   [ ] src/<number>-<project_name>/include/<project_name>/<project_name>.hpp
+#   [ ] src/<number>-<project_name>/src/<project_name>.cpp
+#   [ ] src/<number>-<project_name>/CMakeLists.txt
+#   [ ] tests/<number>-<project_name>/unit_tests.cpp
+#   [ ] tests/<number>-<project_name>/CMakeLists.txt
+#   [ ] record in the src/CMakeLists.txt
+#   [ ] record in the tests/CMakeLists.txt
