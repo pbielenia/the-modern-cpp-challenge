c29b69593070f3d03c5c122f33a1f0761ed7d2eb
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-07-01 22:19:34 +0200

    Create CMakeLists.txt with the script

--- scripts/create_project.py
+++ scripts/create_project.py
@@ -9,26 +9,32 @@ class ProjectCreator(object):
         self.number = number
         self.title = title
         self.class_name = class_name
+        self.full_name = self.__generate_full_name()
         self.project_path = self.__generate_project_path()
+        self.__header_name = self.class_name + '.hpp'
+        self.__source_name = self.class_name + '.cpp'
         self.__include_dir_path = self.__generate_include_dir_path()
         self.__source_dir_path = self.__generate_source_dir_path()
         self.__header_path = self.__generate_header_path()
         self.__source_path = self.__generate_source_path()
 
+    def __generate_full_name(self):
+        return str(self.number).zfill(3) + '-' + self.title
+
     def __generate_project_path(self):
-        return os.getcwd() + '/src/' + str(self.number).zfill(3) + '-' + self.title
+        return os.getcwd() + '/src/' + self.full_name + '/'
 
     def __generate_include_dir_path(self):
-        return self.project_path + '/include/' + self.title
+        return self.project_path + 'include/' + self.title + '/'
 
     def __generate_source_dir_path(self):
-        return self.project_path + '/src'
+        return self.project_path + 'src/'
 
     def __generate_header_path(self):
-        return self.__include_dir_path + '/' + self.class_name + '.hpp'
+        return self.__include_dir_path + self.__header_name
 
     def __generate_source_path(self):
-        return self.__source_dir_path + '/' + self.class_name + '.cpp'
+        return self.__source_dir_path + self.__source_name
 
     def __create_header_file(self):
         header = open(self.__header_path, 'w+')
@@ -48,6 +54,26 @@ class ProjectCreator(object):
                      "\n")
         source.close()
 
+    def __create_cmakelists_file(self):
+        source = open(self.project_path + '/CMakelists.txt', 'w+')
+        source.write("cmake_minimum_required(VERSION 3.17)\n\n"
+                     "set(target_name " + self.full_name + ")\n\n"
+                     "add_library(${target_name} STATIC src/" +
+                     self.__source_name + ")\n"
+                     "target_include_directories(${target_name}\n"
+                     "PUBLIC\n"
+                     "\t$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>\n"
+                     "PRIVATE\n"
+                     "\t$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/" +
+                     self.title + ">\n"
+                     ")\n\n"
+                     "target_compile_options(${target_name}"
+                     "PRIVATE\n"
+                     "\t-Wall -Wextra -pedantic -Werror\n"
+                     ")\n"
+                     )
+        source.close()
+
     def create_directories(self):
         print("Creating directories:\n\t{}\n\t{}".format(
               self.__include_dir_path, self.__source_dir_path))
@@ -59,6 +85,7 @@ class ProjectCreator(object):
               self.__header_path, self.__source_path))
         self.__create_header_file()
         self.__create_source_file()
+        self.__create_cmakelists_file()
 
 
 creator = ProjectCreator(sys.argv[1], sys.argv[2], sys.argv[3])
