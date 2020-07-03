ca4a79f118bfb2b52e4adcc16eb117df7e133749
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-07-03 23:18:25 +0200

    Create project structure

---
+++ src/024-string_to_binary_convertion/src/StrToBinConverter.cpp
@@ -0,0 +1,2 @@
+#include "string_to_binary_convertion/StrToBinConverter.hpp"
+
---
+++ tests/024-string_to_binary_convertion/unit_tests.cpp
@@ -0,0 +1,10 @@
+#include "string_to_binary_convertion/StrToBinConverter.hpp"
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+
+TEST(StrToBinConverter, ExampleTest)
+{
+	auto example = StrToBinConverter();
+}
